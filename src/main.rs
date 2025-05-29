0c53c94d7401ce668f9b636efe7f9ce560c2b661
codingbot24-s <primecoder8@gmail.com>
2025-05-29 08:01:19 +0000

    Refactor process function to improve command handling and add data storage

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,29 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use ::mini_redis::Command::{self, Get, Set};
+    use ::std::collections::HashMap;
+
+    // hashmap for store the data;
+    let mut db = HashMap::new();
+    // connection
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
-
-
